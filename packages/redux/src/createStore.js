258d8c446a0a80254094a07c142252b6d93cfa0c
jbee.han <jbee.han@navercorp.com>
2018-06-11 17:00:36 +0900

    :tada: feat: Add parameter of enhancer to createStore api

--- packages/redux/src/createStore.js
+++ packages/redux/src/createStore.js
@@ -1,13 +1,34 @@
 // Redux supply createStore api for creating store for effective application state management.
 // Developer who will use redux, first of all, create store by this api.
-// This api return `store`.
+// This api return `store` or enhanced `store`.
 // This `store` support some api for managing state of application.
-export function createStore(reducer, state) {
+export function createStore(reducer, preloadedState, enhancer) {
+
+	// * [Prevent Code]
+	// If developer using redux, pass directly enhancer without preloadState,
+	// assign preloadState to enhancer and change preloadState to undefined.
+	if (typeof preloadedState === 'function' && typeof enhancer === 'undefined') {
+		enhancer = preloadedState;
+		preloadedState = undefined;
+	}
+
+	// * [Prevent Code]
+	// If developer using redux, pass enhancer function to createStore api,
+	// return value applied enhancer function.
+	if (typeof enhancer !== 'undefined' && typeof enhancer === 'function') {
+		return enhancer(createStore)(reducer, preloadedState);
+	}
+
+	// * [Prevent Code]
+	// reducer parameter must be function type.
+	if (typeof reducer !== 'function') {
+		throw new Error('Parameter reducer must function type');
+	}
 
 	// This is store for save application state.
 	// Get initial state from each reducer.
 	// Maybe @param reducer is reducer derived from combineReducer.
-	let currentState = reducer(state, {
+	let currentState = reducer(preloadedState, {
 		type: "@@init"
 	});
 
@@ -15,22 +36,33 @@ export function createStore(reducer, state) {
 	// Listeners are added by subscribe api.
 	let listeners = [];
 
-	// Return application state managed by redux.
+	/**
+	 * * [API function]
+	 * Return application state managed by redux.
+	 */
 	function getState() {
 		return currentState;
 	}
 
-	// Developer using redux can modify state by dispatch api.
-	// Developer pass object called action.
-	// Passed action is determine reducer's case.
+	/**
+	 * * [API function]
+	 * Developer using redux can modify state by dispatch api.
+	 * Developer pass object called action.
+	 * Passed action is determine reducer's case.
+	 * @param {Action} action Action object
+	 */
 	function dispatch(action) {
 		currentState = reducer(currentState, action);
 		listeners.forEach(l => l());
 	}
 
-	// Developer using redux can `watch` changed state by using this api.
-	// @param listener is just function.
-	// These listeners are executed when called dispatch.
+	/**
+	 * * [API function]
+	 * Developer using redux can `watch` changed state by using this api.
+	 * These listeners are executed when called dispatch.
+	 * @param {Function} listener
+	 * @returns {Function} This is function for unsubscribing.
+	 */
 	function subscribe(listener) {
 		listeners.push(listener);
 
