ac7f041894c7a21d58e7225ec2680e4a869e2796
jbee.han <jbee.han@navercorp.com>
2018-06-17 14:54:44 +0900

    :tada: feat: Add configureStore API

---
+++ test/demo/src/js/store/configureStore.js
@@ -0,0 +1,15 @@
+import { createStore, combineReducers, applyMiddleware } from "../../../../../packages/redux";
+import { logger } from "../../../../../packages/redux-logger";
+import { countReducer, reposReducer } from "../reducers";
+
+const reducer = combineReducers({
+	count: countReducer,
+	reposReducer,
+});
+
+export default function configureStore() {
+	// const store = createStore(reducer, {}, applyMiddleware(logger));
+	const store = createStore(reducer, applyMiddleware(logger));
+
+	return store;
+}
