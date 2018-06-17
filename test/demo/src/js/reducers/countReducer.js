325628a714b3f816a7c27690a608f65c26a1dae5
jbee.han <jbee.han@navercorp.com>
2018-06-17 15:05:05 +0900

    :wrench: fix: Change variable names

--- test/demo/src/js/reducers/countReducer.js
+++ test/demo/src/js/reducers/countReducer.js
@@ -1,18 +1,18 @@
 import { PLUS_COUNT, MINUS_COUNT } from "../action.js/actionType";
 
 const initialState = {
-	count: 0,
+	number: 0,
 };
 
 export const countReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case PLUS_COUNT:
 			return Object.assign({}, state, {
-				count: state.count + 1,
+				number: state.number + 1,
 			});
 		case MINUS_COUNT:
 			return Object.assign({}, state, {
-				count: state.count - 1,
+				number: state.number - 1,
 			});
 		default:
 			return state;
